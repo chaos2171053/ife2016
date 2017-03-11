2c81b8d366053d314e9667aa8c8c90ab61a6d024
chaos2171053 <chaos2171053@gmail.com>
2017-03-11 11:43:02 +0800

    Revert "Revert "git add .""
    This reverts commit d317ab50922fded70af4f7b432a050e5988c0783.

---
+++ 第三阶段任务/任务41/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Calendar from '../containers/Calendar'
+
+const App = () => (
+    <div>
+        <Calendar/>
+    </div>
+)
+
+export default App
---
+++ 第三阶段任务/任务41/src/components/CalendarHeader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const CalendarHeader = () => (
+    <div>
+        Header
+    </div>
+)
+
+export default CalendarHeader
