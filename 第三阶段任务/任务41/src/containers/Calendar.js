9fad5dfa4f78aaba265e472991877801ec245b55
chaos2171053 <chaos2171053@gmail.com>
2017-03-11 11:50:30 +0800

    静态布局

--- 第三阶段任务/任务41/src/containers/Calendar.js
+++ 第三阶段任务/任务41/src/containers/Calendar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CalendarHeader from '../components/CalendarHeader'
-import CalendarBody from '../components/CalendarBody'
-import CalendarFooter from '../components/CalendarFooter'
+import CalendarHeader from '../components/Calendar/CalendarHeader'
+import CalendarBody from '../components/Calendar/CalendarBody'
+import CalendarFooter from '../components/Calendar/CalendarFooter'
 const Calendar = () => (
     <div>
         <p>2017/3/11</p>
