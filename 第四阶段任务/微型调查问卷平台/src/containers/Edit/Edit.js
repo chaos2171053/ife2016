094b9d04abae2607cedc0ed0e5d9d665264363e2
chaos2171053 <chaos2171053@gmail.com>
2017-03-17 20:03:48 +0800

    配置redux-actions

--- 第四阶段任务/微型调查问卷平台/src/containers/Edit/Edit.js
+++ 第四阶段任务/微型调查问卷平台/src/containers/Edit/Edit.js
@@ -1,14 +1,44 @@
-import React, { PropTypes } from 'react';
-import {Link} from 'react-router-dom'
+import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router-dom'
 
-const Edit = () => (
-    <div>
-    <Link to = '/'> <span>ddd</span></Link>
-       
-    </div>
-)
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as Actions from '../../actions/calendar'
 
-Edit.propTypes = {
-    children: PropTypes.element.isRequired
-};
+// const mapStateToProps = state => ({
+
+//     calendar: state.calendar
+// })
+const mapStateToProps = state => {
+
+    return {
+
+
+    calendar: state.rootReducer.calendar
+
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(Actions, dispatch)
+})
+
+@connect(mapStateToProps, mapDispatchToProps)
+class Edit extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+
+        return (
+            <div>
+                <Link to='/'> <span>ddd</span></Link>
+
+            </div>
+        )
+    }
+}
+// Edit.propTypes = {
+//     children: PropTypes.element.isRequired
+// };
 export default Edit
