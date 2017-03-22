78887c686d59426c0f901b6e998af2f84bec347c
chaos2171053 <chaos2171053@gmail.com>
2017-03-22 20:52:47 +0800

    完成注册页面

--- 第四阶段任务/微型调查问卷平台/src/containers/Login/Login.js
+++ 第四阶段任务/微型调查问卷平台/src/containers/Login/Login.js
@@ -3,7 +3,6 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 import { Link, Redirect } from 'react-router-dom'
 
-// const FormItem = Form.Item;
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as Actions from '../../actions/status'
@@ -29,8 +28,8 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            chooseSignin:false,
-            chooseSignup:true
+            chooseSignin: false,
+            chooseSignup: true
         }
     }
 
@@ -43,82 +42,72 @@ class Login extends Component {
         )
     }
     renderNavs() {
+
         return (
             <div className={styles.navs}>
                 <div className={styles['navs-slider']}>
-                    <a href='#' className={classNames({[styles['active']]:this.state.chooseSignup})}>注册</a>
-                    <a href='#' className={classNames({active:this.state.chooseSignin})}>登录</a>
+                    <a href='#' className={classNames({ [styles['active']]: this.state.chooseSignup })}>注册</a>
+                    <a href='#' className={classNames({ [styles['active']]: this.state.chooseSignin })}>登录</a>
                     <span className={styles['navs-slider-bar']}></span>
+                    <span className={classNames({
+                        [styles["navs-slider-bar"]]: true,
+                        [styles['bar-acitve']]: this.state.chooseSignin,
+                    })}></span>
                 </div>
             </div>
         )
+    }
+    renderSignup() {
+        return (
+            <div className={styles.signup}>
+                <div className={styles['login-box']}>
+                    <div className={styles['group-inputs']}>
+                        <div className={styles['input-wrapper']}>
+                            <input type='text' name='username' placeholder='用户名' />
+                        </div>
+                        <div className={classNames({
+                            [styles['input-wrapper']]:true,
+                            [styles['phonenumber']]:true
+                        })}>                    
+                            <input type='text' name='phonenumber' placeholder='手机号（仅支持中国大陆)' />
+                        </div>
+                        <div className={classNames({
+                            [styles['input-wrapper']]:true,
+                            [styles['password']]:true
+                        })}> 
+                            <input type='password' name='password' placeholder='密码（不少于6位）' />
+                        </div>
+                    </div>
+                    <div className={styles['button-wrapper']}>
+                        <button className={styles['sign-button']}>注册问吧</button>
+                    </div>
+                </div>
+                <p className={styles.agreement}>点击「注册」按钮，即代表你同意
+                                <a href='#'>《豆腐脑是甜的协议》</a>
+                </p>
+            </div>
+        )
+    }
+    renderSignin() {
+        
     }
     render() {
         const { actions: { logIn }, status: { isLogin } } = this.props;
         if (isLogin) {
             return <Redirect to='/home' />
         }
-        // return(
-        //     <div>
-        //     <Button onClick = {logIn}></Button>
-        //     </div>
-        // )
-        // return (
-        //     <div className={styles.main}>
-        //         <div calssName={styles.header}>
-        //             <h1 className={styles.logo}>问吧</h1>
-        //             <h2 className={styles.subtitle}>与世界分享你瞎编的问卷</h2>
-        //         </div>
-        //         <div className={styles['tab-navs']}>
-        //             <a href='#'>注册</a>
-        //             <a href='#'>登录</a>
-        //         </div>
-        //         <div className={styles.register}>
-        //             <div className={styles['group-input']}>
-        //                 <div clsssName={styles.username}>
-        //                     <input type='text' placeholder='用户名' />
-        //                 </div>
-        //                 <div clsssName={styles.phone}>
-        //                     <input type='tel' placeholder='手机号(仅支持中国大陆)' />
-        //                 </div>
-        //                 <div clsssName={styles.password}>
-        //                     <input type='password' placeholder='密码(不少于6位)' />
-        //                 </div>
-        //                 <div>注册问吧</div>
-        //                 <p>点击「注册」按钮，即代表你同意《豆腐脑是甜的的协议》</p>
-        //             </div>
-        //         </div>
-        //     </div>
-        // )
+
         return (
+            <div className = {styles['main-warpper']}>
             <div className={styles.main}>
                 <div className={styles['main-body']}>
                     {this.renderHeader()}
                     <div className={styles.content}>
                         {this.renderNavs()}
-                        <div className={styles.signup}>
-                            <form className={styles['login-box']}>
-                                <div className={styles['group-inputs']}>
-                                    <div className={styles.username}>
-                                        <input type='text' name='username' placeholder='用户名' />
-                                    </div>
-                                    <div className={styles.phonenumber}>
-                                        <input type='text' name='phonenumber' placeholder='手机号（仅支持中国大陆)' />
-                                    </div>
-                                    <div className={styles.password}>
-                                        <input type='password' name='password' placeholder='密码（不少于6位）' />
-                                    </div>
-                                </div>
-                                <div className={styles['button-wrapper']}>
-                                    <button className={styles['sign-button']}>注册问吧</button>
-                                </div>
-                            </form>
-                            <p className={styles.agreement}>点击「注册」按钮，即代表你同意
-                        <a href='#'>《豆腐脑是甜的协议》</a>
-                            </p>
-                        </div>
+                        {this.state.chooseSignup?this.renderSignup():this.renderSignin()}
                     </div>
                 </div>
+            </div>
             </div>
         )
     }
@@ -126,6 +115,6 @@ class Login extends Component {
 
 }
 
-//  Login = Form.create()(Login);
+
 
 export default Login
