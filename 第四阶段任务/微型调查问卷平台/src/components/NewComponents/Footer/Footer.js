b3af98a90951b23bb8e1aee6d32777fa7bc3ea40
chaos2171053 <chaos2171053@gmail.com>
2017-03-26 22:37:03 +0800

    添加日期选择组件

--- 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Footer/Footer.js
+++ 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import moment from 'moment';
+import { DatePicker } from 'antd';
+import styles from './Footer.scss'
+import classNames from 'classnames'
+moment.locale('zh-cn');
 
-const Footer = ()=> {
-    return(
-        <div>
-        footer
+
+const Footer = () => {
+    return (
+        <div className = {classNames({
+            [styles['footer']]:true,
+            [styles['clearfix']]:true
+        })}>
+            <DatePicker/>
+            <button>返回</button>
+            <button>发布问卷</button>
+            <button>保存问卷</button>
         </div>
     )
 }
