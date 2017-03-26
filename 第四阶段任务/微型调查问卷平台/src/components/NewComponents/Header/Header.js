0e5731eafa269a070e3cd623f286b6fe92d8719f
chaos2171053 <chaos2171053@gmail.com>
2017-03-26 20:47:46 +0800

    初步完成header组件样式

--- 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Header/Header.js
+++ 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../../../containers/New/New.scss'
 const Header = () => {
     return (
-        <div className={styles['header-wrapper']}>
+        <div>
             <input className={styles.header} placeholder='请填写标题'></input>
         </div>
     )
