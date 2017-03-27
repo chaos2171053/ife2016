ae97c62991a8890d7651baeaec0182c4063e0d1a
chaos2171053 <chaos2171053@gmail.com>
2017-03-27 11:19:51 +0800

    更改底部布局

--- 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Footer/Footer.js
+++ 第四阶段任务/微型调查问卷平台/src/components/NewComponents/Footer/Footer.js
@@ -7,15 +7,27 @@ moment.locale('zh-cn');
 
 
 const Footer = () => {
+    // const datePickerStyle ={width: '7.5rem',}
+    // return (
+    //     <div className = {classNames({
+    //         [styles['footer']]:true,
+    //         [styles['clearfix']]:true
+    //     })}>
+    //         <DatePicker/>
+    //         <button>返回</button>
+    //         <button>发布问卷</button>
+    //         <button>保存问卷</button>
+    //     </div>
+    // )
     return (
-        <div className = {classNames({
-            [styles['footer']]:true,
-            [styles['clearfix']]:true
-        })}>
-            <DatePicker/>
-            <button>返回</button>
-            <button>发布问卷</button>
-            <button>保存问卷</button>
+        <div className={styles.footer}>
+            <div className={styles['datePicker-wrapper']}>
+                <span className = {styles.deadline}>问卷截止日期:</span>
+                <DatePicker />
+            </div>
+                <button>保存问卷</button>
+                <button>发布问卷</button>
+                <button>返回</button>
         </div>
     )
 }
