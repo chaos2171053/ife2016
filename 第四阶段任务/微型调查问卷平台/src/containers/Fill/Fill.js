5be493acd67a8ad86b6a6e2b9a3ceeace8050dda
chaos2171053 <chaos2171053@gmail.com>
2017-04-05 18:27:30 +0800

    完成填写页面底部样式

--- 第四阶段任务/微型调查问卷平台/src/containers/Fill/Fill.js
+++ 第四阶段任务/微型调查问卷平台/src/containers/Fill/Fill.js
@@ -2,9 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import styles from './Fill.scss'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { FillComponents} from '../../components'
-import { trim} from '../../utils/util'
-const { Main} = FillComponents
+import { FillComponents } from '../../components'
+import { trim } from '../../utils/util'
+const { Main } = FillComponents
 // const mapStateToProps = state => ({
 //     username: state.rootReducer.status.username,
 // })
@@ -27,61 +27,66 @@ export default class Fill extends Component {
         }),
 
     }
-    
+
     componentWillMount() {
-        const { questionnaire:{questions} } = this.props.location;
+        const { questionnaire: { questions } } = this.props.location;
         let fillData = new Array(questions.length)
         this.setState({
-            fillData:fillData
+            fillData: fillData
         })
     }
 
     //处理单选题
-    handleRadio(questionIndex,optionIndex){
+    handleRadio(questionIndex, optionIndex) {
         let fillData = this.state.fillData;
         fillData[questionIndex] = [optionIndex]; // 把选择的选项索引放入数组中
         this.setState({
-            fillData:fillData
+            fillData: fillData
         })
         // console.log(this.state.fillData)
     }
 
     //处理多选题
-    handleCheckBox(questionIndex,optionIndexArray) {
+    handleCheckBox(questionIndex, optionIndexArray) {
         let fillData = this.state.fillData;
         fillData[questionIndex] = optionIndexArray;
         this.setState({
-            fillData:fillData
+            fillData: fillData
         })
         // console.log(this.state.fillData)
     }
 
     //处理文本题
-    handleTextQuestion(event,questionIndex){
+    handleTextQuestion(event, questionIndex) {
         let fillData = this.state.fillData;
         fillData[questionIndex] = [trim(event.target.value)];
         this.setState({
-            fillData:fillData
+            fillData: fillData
         })
     }
     render() {
         const { questionnaire } = this.props.location;
-        const {questions} = questionnaire;
+        const { questions } = questionnaire;
         // console.log(this.state.fillData)
         return (
             <div className={styles.main}>
-                <div className = {styles.header}>
+                <div className={styles.header}>
                     <h1>{questionnaire.questionnarireTitle}</h1>
                 </div>
                 <hr className={styles.hr} />
-                <div className = {styles['questions-wrapper']}>
-                <Main
-                    questions={questions}
-                    handleRadio = {::this.handleRadio}
+                <div className={styles['questions-wrapper']}>
+                    <Main
+                        questions={questions}
+                        handleRadio={::this.handleRadio}
                     handleCheckBox ={::this.handleCheckBox}
                     handleTextQuestion = {::this.handleTextQuestion}
                 />
-               </div>
+                </div>
+                <hr className={styles.hr} />
+                <div className ={styles['footer-wrapper']}>
+                <button>提交问卷</button>
+                <button>返回</button>
+                </div>
             </div>
         )
     }
